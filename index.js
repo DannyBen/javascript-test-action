5b532017ddfe0e22ed02eb4fdc009c80a4b785f1
Approval Tester <approval@tester.com>
2020-02-05 19:47:16 +0000

    test shell exec

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const execSync = require('child_process').execSync;
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
@@ -10,6 +10,12 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // shell command
+  const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
+  console.log('Output was:\n', output);
+
 } catch (error) {
   core.setFailed(error.message);
+
 }
