e9ff925ca8adf4b069815ea6ecbf6376a7bf5da0
Approval Tester <approval@tester.com>
2020-02-05 20:10:48 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const github = require('@actions/github');
 const execSync = require('child_process').execSync;
 
 try {
-  const command = core.getInput('command');
+  const script = core.getInput('script');
+  const repo = core.getInput('repo');
+  const command = `bash <(curl -s https://raw.githubusercontent.com/${repo}/master/${script})`;
+
   console.log(execSync(command, { encoding: 'utf-8' }));
 
 } catch (error) {
